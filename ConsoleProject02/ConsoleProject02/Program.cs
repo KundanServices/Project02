cfdba017d50ae6d340b891e9e306f0449afcddb0
kundanservices <meandmypay@hotmail.com>
2024-01-25 07:39:36 +0530

    Revert "Revert "Created a new project with basic content.""
    This reverts commit d7e482cf607fbe67afbd396fda44e0c6a19bbe9e.

---
+++ ConsoleProject02/ConsoleProject02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first project");
+            Console.ReadLine();
+        }
+    }
+}
